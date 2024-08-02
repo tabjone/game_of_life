7db32df05807c17eb2129e2ba4ef45da2c228075
tabjone <69783177+tabjone@users.noreply.github.com>
2024-08-02 14:20:04 +0200

    Create main.py
    main.py for running Game of Life

---
+++ main.py
@@ -0,0 +1,62 @@
+from game_of_life import GameOfLife
+import pygame as pg
+
+BACKGROUND_COLOR = (213, 213, 213)  # Light grey
+
+
+def main() -> None:
+    """
+    Main function to run Conway's Game of Life
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+
+    # Number of rows and columns in the grid
+    num_rows, num_columns = 300, 300
+
+    # Screen width and height
+    screen_width, screen_height = 200, 250
+
+    # Create and initialize the grid
+    grid = GameOfLife(num_rows, num_columns)
+    grid.initialize_grid_random()
+
+    # Initialize pygame, create the screen and the clock
+    pg.init()
+    pg.display.set_caption("Game of Life")
+    screen = pg.display.set_mode((screen_width, screen_height), pg.RESIZABLE)
+    screen.fill(BACKGROUND_COLOR)
+    clock = pg.time.Clock()
+
+    # Draw the grid to the screen
+    grid.draw_surface(screen, screen_width, screen_height)
+
+    time_since_last_update = 0  # time since the last update in milliseconds
+    time_between_update = 200  # number of milliseconds between each update
+
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                # Quit the game
+                running = False
+            elif event.type == pg.VIDEORESIZE:
+                # Resize the screen
+                screen_width, screen_height = event.w, event.h
+        if time_since_last_update >= time_between_update:
+            # Update the grid and draw it to the screen
+            time_since_last_update = 0
+            grid.update_status()
+            grid.draw_surface(screen, screen_width, screen_height)
+
+        time_since_last_update += clock.tick(60)
+
+    pg.quit()
+
+
+if __name__ == "__main__":
+    main()
